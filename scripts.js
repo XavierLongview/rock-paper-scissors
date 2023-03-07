cf2707ec3008eb778ee3b690aa9fc1a71df94f71
Shane Carroll <scarroll825@gmail.com>
2023-03-07 10:48:40 -0500

    Add determine winner function

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-// Get computer choice: 
-// 1. Get a randome number from 1-3
+// Randomly returned Rock, Paper or Scissors
 let getComputerChoice = () => {
     num = Math.trunc(Math.random() * 3);
     if (num === 0) {
@@ -10,4 +9,36 @@ let getComputerChoice = () => {
         return 'Scissors';
     }
 }
+
+// Determines winnner between player and computer choices 
+let determineWinner = (playerChoice, computerChoice) => {
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (playerChoice === 'rock') {
+        if (computerChoice === 'rock') {
+            return 'Tie game!';
+        } else if (computerChoice === 'paper') {
+            return 'Computer wins!';
+        } else if (computerChoice === 'scissors') {
+            return 'Player wins!';
+        }
+    } else if (playerChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            return 'Player wins!';
+        } else if (computerChoice === 'paper') {
+            return 'Tie game!';
+        } else if (computerChoice === 'scissors') {
+            return 'Computer wins!';
+        }
+    } else if (playerChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'Computer wins!';
+        } else if (computerChoice === 'paper') {
+            return 'Player wins!';
+        } else if (computerChoice === 'scissors') {
+            return 'Tie game!';
+        }
+    }
+}
+
 
